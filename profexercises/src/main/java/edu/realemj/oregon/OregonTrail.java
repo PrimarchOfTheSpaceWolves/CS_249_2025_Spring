4e6e0472f5a4993b4d48dbbd68b9a90ab489e224
Dr. Michael J. Reale <realemj@sunypoly.edu>
2025-02-26 11:52:16 -0500

    PartyState example

--- profexercises/src/main/java/edu/realemj/oregon/OregonTrail.java
+++ profexercises/src/main/java/edu/realemj/oregon/OregonTrail.java
@@ -3,11 +3,15 @@ package edu.realemj.oregon;
 import java.util.*;
 public class OregonTrail {
     public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+
+        PartyState party = PartyState.createParty(input);
+        System.out.println(party);
 
         Supplies supplies = new Supplies();
         supplies.setTotalMoney(300);
         GeneralStore store = new GeneralStore();
-        Scanner input = new Scanner(System.in);
+
         store.enterStore(input, supplies);
         System.out.println(supplies);
     }
---
+++ profexercises/src/main/java/edu/realemj/oregon/PartyState.java
@@ -0,0 +1,52 @@
+package edu.realemj.oregon;
+
+import java.util.*;
+public class PartyState {
+    private Person [] party;
+
+    public PartyState(String [] names) {
+        party = new Person[names.length];
+        for(int i = 0; i < party.length; i++) {
+            party[i] = new Person(names[i]);
+        }
+    }
+
+    public int getPartyCnt() {
+        return party.length;
+    }
+
+    public int getAliveCnt() {
+        int cnt = 0;
+        for(Person p: party) {
+            if(p.isAlive()) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("PARTY:\n");
+        for(Person p: party) {
+            sb.append("* ");
+            sb.append(p);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static PartyState createParty(Scanner input) {
+        System.out.println("Enter party cnt:");
+        String cntStr = input.nextLine();
+        int cnt = Integer.parseInt(cntStr);
+        String [] names = new String[cnt];
+        System.out.println("Enter names:");
+        for(int i = 0; i < names.length; i++) {
+            names[i] = input.nextLine();
+        }
+        PartyState party = new PartyState(names);
+        return party;
+    }
+
+}
