d61e4899ac118a908068efe71586e680ac9cd830
Dr. Michael J. Reale <realemj@sunypoly.edu>
2025-03-31 11:51:09 -0400

    Started on exception handling

---
+++ profexercises/src/main/java/edu/realemj/exercises13/Quotient.java
@@ -0,0 +1,40 @@
+package edu.realemj.exercises13;
+import java.util.*;
+import java.io.*;
+public class Quotient {
+    public static void incrediblyStupidFunction() throws IOException {
+        throw new IOException("WHOOPS");
+    }
+    public static int divide(int a, int b) {
+        if(b == 0) {
+            throw new ArithmeticException("THE FORBIDDEN MATH!!!");
+        }
+
+        return a / b;
+    }
+
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter two numbers:");
+            int x = input.nextInt();
+            int y = input.nextInt();
+            int z = divide(x, y);
+            System.out.println("Answer = " + z);
+            incrediblyStupidFunction();
+        }
+        catch(ArithmeticException ae) {
+            System.out.println("CAUGHT MATH ISSUES.");
+        }
+        catch(Exception e) {
+            System.out.println("SOMETHING ELSE BAD...");
+        }
+        /*
+        catch(InputMismatchException ie) {
+            System.out.println("WHAT KIND OF NUMBERS ARE THESE???");
+        }*/
+
+        System.out.println("PROGRAM OVER");
+    }
+}
